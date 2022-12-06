eec41c3879ab8ba08e218cebea8fd9c5bdc3ae8b
Edain <101297627+Astralzz@users.noreply.github.com>
2022-12-05 22:17:29 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'plantilla1_uagro',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
